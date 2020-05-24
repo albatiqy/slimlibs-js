cdf7ed298abd7c127e72a8453572bd671cb2deb4
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-05-25 01:00:21 +0700

    dsfdsf

--- xapp/tinymce.js
+++ xapp/tinymce.js
@@ -35,69 +35,77 @@ xApp.tinymce = function(selector, config) {
         libdef = {
             target: $element,
             menubar: false,
-            plugins:
-                'advlist autolink lists link image charmap print preview anchor ' +
+            plugins: 'advlist autolink lists link image charmap print preview anchor ' +
                 'searchreplace visualblocks code fullscreen ' +
-                'insertdatetime media table paste help wordcount'
-                ,
+                'insertdatetime media table paste help wordcount',
             toolbar: 'undo redo | formatselect | ' +
                 'bold italic underline strikethrough | alignleft aligncenter ' +
-                'alignright alignjustify | '+
+                'alignright alignjustify | ' +
                 'bullist numlist outdent indent | link image-browser media insert-pdf | ' +
                 'fullscreen preview code | ' +
                 'removeformat | help',
+            media_url_resolver: function(data, resolve, reject) {
+                xApp.apiGet('/api/v0/module/media/embedcode?url=' + decodeURI(data.url))
+                    .then(json => {
+                        if (json.data.result) {
+                            resolve({ html: json.data.code })
+                        } else {
+                            reject({msg: 'invalid resource'})
+                        }
+                    })
+            },
             images_upload_handler: imageUploadHandler,
             relative_urls: false,
             setup: function(editor) {
-                    editor.ui.registry.addButton('image-browser', {
-                        icon: 'gallery',
-                        tooltip: 'Image Browser',
-                        onAction: function() {
-                            editor.windowManager.openUrl({
-                                title: 'Image Browser',
-                                url: xApp.basePath + '/modules/tinymce/media-browser-dlg?filter=image',
-                                height: 600,
-                                width: 1000,
-                                onMessage: function(instance, data) {
-                                    switch (data.mceAction) {
-                                        case 'filePicked':
-                                            if (data.fileType=='IMAGE') {
-                                                editor.execCommand('mceInsertContent', false, '<img src="' + settings.mediaBasePath + data.fileSrc + '">')
-                                            }
-                                            instance.close()
-                                    }
+                editor.ui.registry.addButton('image-browser', {
+                    icon: 'gallery',
+                    tooltip: 'Image Browser',
+                    onAction: function() {
+                        editor.windowManager.openUrl({
+                            title: 'Image Browser',
+                            url: xApp.basePath + '/modules/tinymce/media-browser-dlg?filter=image',
+                            height: 600,
+                            width: 1000,
+                            onMessage: function(instance, data) {
+                                switch (data.mceAction) {
+                                    case 'filePicked':
+                                        if (data.fileType == 'IMAGE') {
+                                            editor.execCommand('mceInsertContent', false, '<img src="' + settings.mediaBasePath + data.fileSrc + '">')
+                                        }
+                                        instance.close()
                                 }
-                            });
-                        }
-                    })
-                    editor.ui.registry.addButton('insert-pdf', {
-                        icon: 'new-document',
-                        tooltip: 'Sisipkan PDF',
-                        onAction: function() {
-                            editor.windowManager.openUrl({
-                                title: 'PDF Browser',
-                                url: xApp.basePath + '/modules/tinymce/media-browser-dlg?filter=pdf',
-                                height: 600,
-                                width: 1000,
-                                onMessage: function(instance, data) {
-                                    switch (data.mceAction) {
-                                        case 'filePicked':
-                                            if (data.fileType=='PDF') {
-                                                editor.execCommand('mceInsertContent', false, '<iframe class="xapp-pdf-media" src="'+xApp.basePath+'/node_modules/slimlibs-js/libs/pdf.js/web/viewer.html?file='+ encodeURIComponent(settings.mediaBasePath + data.fileSrc) +'"></iframe>')
-                                            }
-                                            instance.close()
-                                    }
+                            }
+                        });
+                    }
+                })
+                editor.ui.registry.addButton('insert-pdf', {
+                    icon: 'new-document',
+                    tooltip: 'Sisipkan PDF',
+                    onAction: function() {
+                        editor.windowManager.openUrl({
+                            title: 'PDF Browser',
+                            url: xApp.basePath + '/modules/tinymce/media-browser-dlg?filter=pdf',
+                            height: 600,
+                            width: 1000,
+                            onMessage: function(instance, data) {
+                                switch (data.mceAction) {
+                                    case 'filePicked':
+                                        if (data.fileType == 'PDF') {
+                                            editor.execCommand('mceInsertContent', false, '<iframe class="xapp-pdf-media" src="' + xApp.basePath + '/node_modules/slimlibs-js/libs/pdf.js/web/viewer.html?file=' + encodeURIComponent(settings.mediaBasePath + data.fileSrc) + '"></iframe>')
+                                        }
+                                        instance.close()
                                 }
-                            });
-                        }
-                    })
-                },
-                content_css: xApp.basePath + '/css/tinymce-content.css'
+                            }
+                        });
+                    }
+                })
+            },
+            content_css: [xApp.basePath + '/node_modules/bootstrap3/dist/css/bootstrap.min.css', xApp.basePath + '/css/tinymce-content.css']
         },
         libsettings = Object.assign(libdef, settings.libSettings)
 
     tinymce.init(libsettings)
-    xApp.onUnload(()=>{
+    xApp.onUnload(() => {
         tinymce.remove()
     })
     return tinymce.get($element.id)
