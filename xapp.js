a3c5dd5b24627f8711f91da68a73aa4267d0185c
Ahmad Fauzi <fauzi.ahmad@gmail.com>
2020-06-20 21:29:42 +0700

    sdfsdf

--- xapp.js
+++ xapp.js
@@ -1,7 +1,8 @@
-import {globals, SlimlibsHandleHttpJSONResponse} from "../../js/modules/globals.js?module"
-const xApp = Object.assign(globals, {
+import {SlimlibsGlobals, SlimlibsHandleHttpJSONResponse} from "../../js/modules/globals.js?module"
+const xApp = Object.assign(SlimlibsGlobals, {
     appSelector: '#app',
     pageSelector: '#container',
+    cookiePath: (this.basePath==''?'/':this.basePath),
     Libs: {}
 })
 
@@ -16,6 +17,27 @@ xApp.notifyError = function(error) {
         $app.dispatchEvent(event)
     }
 }
+
+xApp.cookieEnabled = function() {
+    if (navigator.cookieEnabled) return true
+    document.cookie = "cookietest=1"
+    const ret = document.cookie.indexOf("cookietest=") != -1
+    document.cookie = "cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT"
+    return ret
+}
+
+xApp.getCookie = function(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? decodeURIComponent(v[2]) : null;
+}
+
+xApp.setCookie = function(name, value, seconds) {
+    var d = new Date;
+    d.setTime(1000*seconds);
+    document.cookie = name + "=" + encodeURIComponent(value) + ";path="+xApp.cookiePath+";expires=" + d.toGMTString();
+}
+
+xApp.deleteCookie = function(name) { xApp.setCookie(name, '', -1); }
 
 xApp.routerBasePath = (location.pathname.slice(-1)=="/"?"":location.pathname).substr(xApp.basePath.length)
 
